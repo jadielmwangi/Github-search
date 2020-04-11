bd36b00d661c2c3be4bf7111f520042fa84ef521
jadielmwangi <jedielmwangi@gmail.com>
2020-04-11 18:14:37 +0300

    adds datecount pipe that returns the no. of days

---
+++ src/app/date-count.pipe.ts
@@ -0,0 +1,51 @@
+// import { Pipe, PipeTransform } from '@angular/core';
+
+// @Pipe({
+//   name: 'dateCount'
+// })
+// export class DateCountPipe implements PipeTransform {
+
+//   transform(value: unknown, ...args: unknown[]): unknown {
+//     return null;
+//   }
+
+// }
+
+
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({
+    name: 'dateAgo',
+    pure: true
+})
+export class  DateCountPipe  implements PipeTransform {
+
+    transform(value: any, args?: any): any {
+        if (value) {
+            const seconds = Math.floor((+new Date() - +new Date(value)) / 1000);
+            if (seconds < 59)
+                return 'Just now';
+            const intervals = {
+                'year': 31536000,
+                'month': 2592000,
+                'week': 604800,
+                'day': 86400,
+                'hour': 3600,
+                'minute': 60,
+                'second': 1
+            };
+            let count;
+            for (const j in intervals) {
+                count = Math.floor(seconds / intervals[j]);
+                if (count > 0)
+                    if (count === 1) {
+                        return count + ' ' + j + ' ago';
+                    } else {
+                        return count + ' ' + j + 's ago';
+                    }
+            }
+        }
+        return value;
+    }
+
+}
