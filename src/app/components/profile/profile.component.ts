3adca9197058a1c38e4600d09c45987a02938412
jadielmwangi <jedielmwangi@gmail.com>
2020-04-11 18:11:03 +0300

    generates navbar,profile and repo which are basic units for building our app

---
+++ src/app/components/profile/profile.component.ts
@@ -0,0 +1,50 @@
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-profile',
+//   templateUrl: './profile.component.html',
+//   styleUrls: ['./profile.component.css']
+// })
+// export class ProfileComponent implements OnInit {
+
+//   constructor() { }
+
+//   ngOnInit(): void {
+//   }
+
+// }
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../../services/http.service';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+
+
+  profile: any;
+  repos: any;
+  username:string;
+
+  constructor(private httpService:HttpService){
+
+  }
+
+  findProfile(){
+    this.httpService.updateProfile(this.username);
+    this.httpService.getProfileInfo().subscribe(profile => this.profile = profile);
+
+    this.httpService.getProfileRepos().subscribe(repos => this.repos = repos);
+  }
+
+  ngOnInit() {
+    this.httpService.updateProfile('jadielmwangi');
+    this.httpService.getProfileInfo().subscribe(profile => this.profile = profile);
+
+    this.httpService.getProfileRepos().subscribe(repos => this.repos = repos);
+  }
+
+}
---
+++ src/app/components/repo/repo.component.ts
@@ -0,0 +1,42 @@
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-repo',
+//   templateUrl: './repo.component.html',
+//   styleUrls: ['./repo.component.css']
+// })
+// export class RepoComponent implements OnInit {
+
+//   constructor() { }
+
+//   ngOnInit(): void {
+//   }
+
+// }
+
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from 'src/app/services/http.service';
+
+@Component({
+  selector: 'app-repo',
+  templateUrl: './repo.component.html',
+  styleUrls: ['./repo.component.css']
+})
+export class RepoComponent implements OnInit {
+  repoitems: any[];
+  repoName:string= "pizza";
+
+  constructor(private httpService: HttpService) {
+
+  }
+
+  findRepo(){
+    this.httpService.updateProfile(this.repoName);
+    this.httpService.getProfileRepos().subscribe(repo => this.repoitems= repo["items"]);
+  }
+
+  ngOnInit() {
+    this.httpService.updateProfile(this.repoName);
+    this.httpService.getProfileRepos().subscribe(repo => this.repoitems= repo["items"]);
+  }
+}
