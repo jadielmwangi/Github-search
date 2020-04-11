761f0727b2527b14dda289367ee3aef24f80b737
jadielmwangi <jedielmwangi@gmail.com>
2020-04-11 18:04:53 +0300

    imports components,pipes,classes and directives to enhance functionality of app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import { HttpClientModule } from '@angular/common/http';
+
+import { FormsModule } from '@angular/forms';
+import { HttpService } from './services/http.service';
+
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { HighlightDirective } from './highlight.directive';
+import { DateCountPipe } from './date-count.pipe';
+import { RepoComponent } from './components/repo/repo.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    HighlightDirective,
+    DateCountPipe,
+    RepoComponent,
+    NavbarComponent,
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [HttpService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
