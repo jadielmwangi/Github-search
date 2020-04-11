8fef3af3981c890150514ad937a0e7362113fa48
jadielmwangi <jedielmwangi@gmail.com>
2020-04-11 18:07:33 +0300

    adds apiUrl from the github which gives personal access tokens

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: " 1fd9c8e0d43f150d7b898bbcb86007b209e50aca "
 };
 
 /*
@@ -14,3 +15,4 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+ 
